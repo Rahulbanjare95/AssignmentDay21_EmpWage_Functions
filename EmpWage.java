9eb1d8e2c06b062848b5ebea87b5e60d86edfb4f
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-05-23 14:23:07 +0530

    Added Switch Cases to Switch between Full Time and Part Time

--- EmpWage.java
+++ EmpWage.java
@@ -1,12 +1,13 @@
 public class EmpWage{
 
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;
+	public static final int EMP_RATE_PER_HR=20;
+
 
 	public static void main(String args[] ){
 	System.out.println("Welcome");
 
-	int IS_FULL_TIME=1;
-	int IS_PART_TIME=2;
-	int EMP_RATE_PER_HR=20;
 	int	is_Present=1;
 	int	empHrs=0;
 
@@ -38,6 +39,25 @@ public class EmpWage{
         System.out.println("Emp Wage daily "+ empWage);
 
 //--------------New Branch uC 4-------------
+		
+                switch (attendence)
+        {
+                case IS_FULL_TIME:
+                        empHrs=8;
+                        break;
+                case IS_PART_TIME:
+                        empHrs=4;
+                        break;
+                default:
+                        empHrs=0;
+                                break;
+        }
+		empWage = (empHrs*EMP_RATE_PER_HR);
+        System.out.println("Emp Wage Using Case  "+ empWage);
+
+
+
+
 
 
 
