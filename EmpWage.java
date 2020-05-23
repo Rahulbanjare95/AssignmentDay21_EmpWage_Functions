8ee0d5f6d228b5f7a5e48514eec38ebc3e80fcd9
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-05-23 14:07:45 +0530

    Added Part time Feature

--- EmpWage.java
+++ EmpWage.java
@@ -5,27 +5,39 @@ public class EmpWage{
 	System.out.println("Welcome");
 
 	int IS_FULL_TIME=1;
+	int IS_PART_TIME=2;
 	int EMP_RATE_PER_HR=20;
 	int	is_Present=1;
 	int	empHrs=0;
-	int attendence=(int) Math.floor(Math.random()*10%2+1);
 
+//-------------New Branch UC1------------
+	int attendence=(int) Math.floor(Math.random()*10)%3;
 
 	if(attendence ==is_Present){
 	System.out.println("Present");
 	}
-	else{
-	System.out.println("Absent");
+	else if(attendence ==2){
+	System.out.println("Part-Time");
 	}
+	else { System.out.println("Absent");}
 
+//------------New Branch UC2--------------
 	  if(attendence==IS_FULL_TIME)
         {
         empHrs=8;
         }
 		int empWage=(empHrs*EMP_RATE_PER_HR);
-		System.out.println("EmpWage "+empWage);
+		System.out.println("EmpWage Full-Time "+empWage);
+//-----------New Branch UC 3-------
+		if(attendence==IS_FULL_TIME)
+                empHrs=8;
+        else if(attendence==IS_PART_TIME)
+                empHrs=4;
 
+        empWage = (empHrs*EMP_RATE_PER_HR);
+        System.out.println("Emp Wage daily "+ empWage);
 
+//--------------New Branch uC 4-------------
 
 
 
