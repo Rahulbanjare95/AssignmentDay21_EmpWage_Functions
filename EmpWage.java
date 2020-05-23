a3fa709c099b25f06220ad4255edde1773fec40c
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-05-23 14:39:54 +0530

    Added Feature to Calculate wage till Max hrs allowe and Max Working Days reached

--- EmpWage.java
+++ EmpWage.java
@@ -3,7 +3,11 @@ public class EmpWage{
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
 	public static final int EMP_RATE_PER_HR=20;
-	 public static final int NUM_WORKING_DAYS=20;
+	public static final int NUM_WORKING_DAYS=20;
+
+	public static final int MAX_WORKING_DAYS=100;
+   public static final int  MAX_HRS_IN_MONTH=10;
+
 
 
 
@@ -80,9 +84,38 @@ public class EmpWage{
         }
 			System.out.println("Total Wage For a month - "+totalEmpWage);
 
+//------------------------New Branch UC 6------------------------------
 
+		  int totalWorkingDays=0;
+        int totalWorkingHrs=0;
 
+			
+        while(totalWorkingDays<=MAX_WORKING_DAYS &&
+                totalWorkingHrs<MAX_HRS_IN_MONTH)
+{
+  
 
+        totalWorkingDays++;
+        int empCheck= (int) (Math.floor(Math.random()*10)%2+1);
+
+                switch (empCheck)
+                {
+                case IS_FULL_TIME:
+                        empHrs=8;
+                        break;
+                case IS_PART_TIME:
+                        empHrs=4;
+                        break;
+                default:
+                        empHrs=0;
+                                break;
+                }
+
+        totalWorkingHrs+=empHrs;
+         empWage = (empHrs*EMP_RATE_PER_HR);
+        totalEmpWage+=empWage;
+        }
+		System.out.println("Total Wage till 100 hrs and 20 Working days ="+ totalEmpWage);
 
 	}
 }
