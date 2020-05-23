81562d5a80276ce1da16d21ab1db23840f990b36
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-05-23 12:54:26 +0530

    Attendence Checker Added

--- EmpWage.java
+++ EmpWage.java
@@ -4,5 +4,16 @@ public class EmpWage{
 	public static void main(String args[] ){
 	System.out.println("Welcome");
 
+	int	is_Present=1;
+	int attendence=(int) Math.floor(Math.random()*10%2+1);
+
+
+	if(attendence ==is_Present){
+	System.out.println("Present");
+	}
+	else{
+	System.out.println("Absent");
+	}
+
 	}
 }
