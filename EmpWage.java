eca22f9ac2f32a3bdb324f3423acc8d0ffcc5888
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-05-23 13:58:20 +0530

    Added EmpWage if Emp Present

--- EmpWage.java
+++ EmpWage.java
@@ -4,7 +4,10 @@ public class EmpWage{
 	public static void main(String args[] ){
 	System.out.println("Welcome");
 
+	int IS_FULL_TIME=1;
+	int EMP_RATE_PER_HR=20;
 	int	is_Present=1;
+	int	empHrs=0;
 	int attendence=(int) Math.floor(Math.random()*10%2+1);
 
 
@@ -14,6 +17,17 @@ public class EmpWage{
 	else{
 	System.out.println("Absent");
 	}
+
+	  if(attendence==IS_FULL_TIME)
+        {
+        empHrs=8;
+        }
+		int empWage=(empHrs*EMP_RATE_PER_HR);
+		System.out.println("EmpWage "+empWage);
+
+
+
+
 
 	}
 }
